34ead5799cb55114772179735237501b0d80bf40
Jan Portisch <janportisch@Jans-MBP.fritz.box>
2020-03-15 13:00:40 +0100

    Duration Delta refactored for java 8 + unit test

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,6 +7,7 @@ import training.Word2VecConfiguration;
 import java.io.File;
 import java.time.Duration;
 import java.time.Instant;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Mini command line tool for server application.
@@ -186,17 +187,8 @@ public class Main {
             Instant before = Instant.now();
             rdf2VecLight.train();
             Instant after = Instant.now();
-
-            long days = Duration.between(before, after).toDaysPart();
-            long hours = Duration.between(before, after).toHoursPart();
-            long minutesPart = Duration.between(before, after).toMinutesPart();
-            long seconds = Duration.between(before, after).toSecondsPart();
-
             System.out.println("Training completed.");
-            System.out.println("Days: " + days);
-            System.out.println("Hours: " + hours);
-            System.out.println("Minutes: " + minutesPart);
-            System.out.println("Seconds: " + seconds);
+            System.out.println(getDeltaTimeString(before, after));
         }
     }
 
@@ -219,6 +211,40 @@ public class Main {
         if (positionSet != -1 && arguments.length >= positionSet + 1) {
             return arguments[positionSet + 1];
         } else return null;
+    }
+
+    /**
+     * Helper method. Formats the time delta between {@code before} and {@code after} to a string with human readable
+     * time difference in days, hours, minutes, and seconds.
+     * @param before Start time instance.
+     * @param after End time instance.
+     * @return Human-readable string.
+     */
+    public static String getDeltaTimeString(Instant before, Instant after){
+
+        // unfortunately Java 1.9 which is currently incompatible with coveralls maven plugin...
+        //long days = Duration.between(before, after).toDaysPart();
+        //long hours = Duration.between(before, after).toHoursPart();
+        //long minutes = Duration.between(before, after).toMinutesPart();
+        //long seconds = Duration.between(before, after).toSecondsPart();
+
+        Duration delta = Duration.between(before, after);
+
+        long days = delta.toDays();
+        long hours = days > 0 ? delta.toHours() % (days * 24) : delta.toHours();
+
+
+        long minutesModuloPart = days * 24 * 60 + hours * 60;
+        long minutes = minutesModuloPart > 0 ? delta.toMinutes() % (minutesModuloPart) : delta.toMinutes();
+
+        long secondsModuloPart = days * 24 * 60 * 60 + hours * 60 * 60 + minutes * 60;
+        long seconds = secondsModuloPart > 0 ? TimeUnit.MILLISECONDS.toSeconds(delta.toMillis()) % (secondsModuloPart) : TimeUnit.MILLISECONDS.toSeconds(delta.toMillis());
+
+        String result = "Days: " + days + "\n";
+        result += "Hours: " + hours + "\n";
+        result += "Minutes: " + minutes + "\n";
+        result += "Seconds: " + seconds + "\n";
+        return result;
     }
 
 
